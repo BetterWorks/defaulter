63b7cf9a29febce3dabfe41a40323efa2c977890
Matt Rasmus <mrasmus@betterworks.com>
2015-10-12 17:58:35 -0700

    Add pie chart generation

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use('/', require('./routes/index'));
 app.use('/horseshoe', require('./routes/horseshoe'));
+app.use('/pie', require('./routes/pie'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
